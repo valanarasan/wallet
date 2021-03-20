0a82955e8551cba0d04146389d9d8076999db55b
Valan Arasan <avalanarasan@gmail.com>
2021-03-20 14:24:37 +0530

    [Valan] Add.Logic For Returning Total Wallet Balance in Rupees

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -2,13 +2,38 @@ public class Wallet {
     private final double dollar;
     private final double rupees;
     private final double onedollarequivalentinrupees=74.85;
+    private String prefferedCurrency;
+
+    public Wallet(double rupees, double dollar,String prefferedCurrency) {
+        this.dollar = dollar;
+        this.rupees = rupees;
+        this.prefferedCurrency=prefferedCurrency;
+    }
 
     public Wallet(double rupees, double dollar) {
-        this.rupees=rupees;
-        this.dollar=dollar;
+        this.dollar = dollar;
+        this.rupees = rupees;
     }
 
     public boolean equivalent() {
         return dollar==(rupees/onedollarequivalentinrupees);
+    }
+    public double convertDollarToRupees(){
+        return dollar*onedollarequivalentinrupees;
+    }
+    public int convertRupeesToDollar(){
+        return (int) ((int) rupees/onedollarequivalentinrupees);
+    }
+
+    public double balanceInPrefferedCurrency() {
+        if(prefferedCurrency=="Rs"){
+            double sum=rupees+convertDollarToRupees();
+            return sum;
+        }
+        else{
+            int sum= (int) (dollar+convertRupeesToDollar());
+            return sum;
+        }
+
     }
 }
--- src/test/java/WalletTest.java
+++ src/test/java/WalletTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WalletTest {
@@ -17,4 +16,17 @@ public class WalletTest {
 
     }
 
+    @Test
+    void ReturnWalletBalanceInRupeesAsPreferredCurrency() {
+       String prefferedCurrency="Rs";
+        double dollar=1;
+        double rupees=50;
+        double actual=124.85;
+        Wallet wallet = new Wallet(rupees,dollar,prefferedCurrency);
+
+        double expected=wallet.balanceInPrefferedCurrency();
+
+        assertEquals(expected,actual);
+
+    }
 }
